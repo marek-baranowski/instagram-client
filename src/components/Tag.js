78e9af4267e5068cd135b711925b1fec331d8896
Marek Baranowski <mbrnwsk@gmail.com>
2016-02-17 10:29:41 +0100

    Tag component code cleanup

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -5,12 +5,19 @@ import * as C from '../common/consts';
 
 class Tag extends React.Component {
   render() {
-    return <button onClick={() => this.props.dispatch(
-    routeActions.push({
-      pathname: C.ROUTES.PHOTOS_BY_TAG,
-      search: '?tag=' + this.props.tag
-}))}>#{this.props.tag}</button>;
+    return <button onClick={this.props.tagClick}>#{this.props.tag}</button>;
   }
 }
 
-export default connect()(Tag)
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    tagClick: () => {
+      dispatch(routeActions.push({
+        pathname: C.ROUTES.PHOTOS_BY_TAG,
+        search: '?tag=' + ownProps.tag
+      }))
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Tag)
