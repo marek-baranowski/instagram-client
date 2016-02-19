2200442d88022035aadbebe1302ea51fe9638f6a
Marek Baranowski <mbrnwsk@gmail.com>
2016-02-19 03:17:42 +0100

    Extract API endpoints to separate module

--- src/actions/CommentActions.js
+++ src/actions/CommentActions.js
@@ -1,21 +1,14 @@
 import {createAction} from 'redux-actions';
 import {bind} from 'redux-effects';
 import {fetch} from 'redux-effects-fetch';
+import {comments} from '../common/APIendpoints';
 
 const startFetchingComments = createAction('COMMENTS_ARE_LOADING');
 const setComments = createAction('SET_COMMENTS');
 
-function toYQL(url) {
-  var yqlUrl = 'http://query.yahooapis.com/v1/public/yql?q=',
-    query = 'select * from json where url="{url}"'.replace('{url}', url);
-
-  return yqlUrl + encodeURIComponent(query) + '&format=json';
-}
-
 export function fetchComments(photoId) {
   return [
     startFetchingComments(),
-    bind(fetch(toYQL('https://api.instagram.com/v1/media/' + photoId + '/comments?client_id=a778c35c48824baca6837071dba766df')),
-      ({value}) => setComments(value.query.results.json.data))
+    bind(fetch(comments(photoId)), ({value}) => setComments(value.query.results.json.data))
   ]
 }
--- src/actions/ImageActions.js
+++ src/actions/ImageActions.js
@@ -1,29 +1,21 @@
 import {createAction} from 'redux-actions';
 import {bind} from 'redux-effects';
 import {fetch} from 'redux-effects-fetch';
+import {imagesByTag, imageById} from '../common/APIendpoints';
 
 const startFetchingImages = createAction('IMAGES_ARE_LOADING');
 const setImages = createAction('SET_IMAGES');
 
-function toYQL(url) {
-  var yqlUrl = 'http://query.yahooapis.com/v1/public/yql?q=',
-    query = 'select * from json where url="{url}"'.replace('{url}', url);
-
-  return yqlUrl + encodeURIComponent(query) + '&format=json';
-}
-
 export function fetchImagesByTag(tag) {
   return [
     startFetchingImages(),
-    bind(fetch(toYQL('https://api.instagram.com/v1/tags/' + tag + '/media/recent?client_id=a778c35c48824baca6837071dba766df')),
-      ({value}) => setImages(value.query.results.json.data))
+    bind(fetch(imagesByTag(tag)), ({value}) => setImages(value.query.results.json.data))
   ]
 }
 
 export function fetchImageById(imageId) {
   return [
     startFetchingImages(),
-    bind(fetch(toYQL('https://api.instagram.com/v1/media/' + imageId + '?client_id=a778c35c48824baca6837071dba766df')),
-      ({value}) => setImages(value.query.results.json.data))
+    bind(fetch(imageById(imageId)), ({value}) => setImages(value.query.results.json.data))
   ]
 }
